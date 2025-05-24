beac89b56141db3da3439a71c4882a5d1d325993
Robert Ferguson <rmferguson@protonmail.com>
2025-05-24 13:03:41 -0700

    move timer tests

---
+++ tests/test_sg_timer.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+import pytest
+
+from stargazers.sg_timer import Timer, TimerError
+
+TIMER_STATIC_DURATION = 0.5
+
+
+def test_timer_functionality():
+    with Timer() as t:
+        sleep(TIMER_STATIC_DURATION)
+
+    assert not list(t.get_laps())
+    assert t.start_time
+    assert t.stop_time
+
+
+def test_bad_stop():
+    with pytest.raises(TimerError):
+        t = Timer()
+        t.stop()
+    assert True
